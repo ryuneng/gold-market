22a269f06253b6bd0858ab39ef1bc93c80efaed7
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-04 16:01:01 +0900

    Initial commit

---
+++ resource-server/src/test/java/wantedgold/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,14 @@
+package wantedgold.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
