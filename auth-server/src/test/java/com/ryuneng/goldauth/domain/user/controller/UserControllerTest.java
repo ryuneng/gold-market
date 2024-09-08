fea2503b7d16d6bac87942387aeef1311284a6de
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-08 18:10:29 +0900

    [Test] 회원가입 기능 Controller 예외케이스 테스트 (#11)

--- auth-server/src/test/java/com/ryuneng/goldauth/domain/user/controller/UserControllerTest.java
+++ auth-server/src/test/java/com/ryuneng/goldauth/domain/user/controller/UserControllerTest.java
@@ -49,6 +49,96 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.message").value("회원가입이 완료되었습니다."));
     }
 
+    @DisplayName("유효 범위를 벗어난 길이의 아이디로 회원가입을 시도한다.")
+    @Test
+    void signupWithWrongLengthUsername() throws Exception {
+        // given
+        UserCreateRequest request = createUserCreateRequestBuilder("u", "test1234", "01012345678");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/users")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("아이디는 5자에서 20자 사이여야 합니다."));
+    }
+
+    @DisplayName("형식에 맞지 않는 아이디로 회원가입을 시도한다.")
+    @Test
+    void signupWithWrongUsername() throws Exception {
+        // given
+        UserCreateRequest request = createUserCreateRequestBuilder("User!", "test1234", "01012345678");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/users")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("아이디는 영문 소문자, 숫자, 특수기호(-, _)만 사용 가능합니다."));
+    }
+
+    @DisplayName("유효 범위를 벗어난 길이의 비밀번호로 회원가입을 시도한다.")
+    @Test
+    void signupWithWrongLengthPassword() throws Exception {
+        // given
+        UserCreateRequest request = createUserCreateRequestBuilder("user1", "test", "01012345678");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/users")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("비밀번호는 8자에서 16자 사이여야 합니다."));
+    }
+
+    @DisplayName("형식에 맞지 않는 비밀번호로 회원가입을 시도한다.")
+    @Test
+    void signupWithWrongPassword() throws Exception {
+        // given
+        UserCreateRequest request = createUserCreateRequestBuilder("user1", "testtest", "01012345678");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/users")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("비밀번호는 영문 대/소문자, 숫자, 특수문자 중 2가지 이상을 포함해야 합니다."));
+    }
+
+    @DisplayName("형식에 맞지 않는 전화번호로 회원가입을 시도한다.")
+    @Test
+    void signupWithWrongPhoneNumber() throws Exception {
+        // given
+        UserCreateRequest request = createUserCreateRequestBuilder("user1", "test1234", "010-1234-5678");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/users")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("전화번호는 하이픈(-)을 제외한 숫자만 입력해주세요. (예: 01012345678)"));
+    }
+
     // 유저 Request DTO 빌더 생성
     private UserCreateRequest createUserCreateRequestBuilder(String username, String password, String phoneNumber) {
 
