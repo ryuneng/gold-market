0a8c7c1a7af8cb005923cdc189903ba20df2f101
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-08 12:16:09 +0900

    [Feat] SecurityConfig 클래스 생성 (#9)

---
+++ auth-server/src/main/java/com/ryuneng/goldauth/global/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.ryuneng.goldauth.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((AbstractHttpConfigurer::disable)) // CSRF 보호 기능 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, ("/api/users/**")).permitAll() // 해당 경로 POST 요청은 인증 없이 허용
+                        .anyRequest().authenticated());                                  // 이외의 모든 요청은 인증 필요
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션을 STATELESS 상태로 설정
+
+        return http.build(); // 설정한 보안 필터 체인 반환
+    }
+
+}
