666cc75d4c79650fe2c7a2d6a42a05ec5a877f33
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-11 19:11:58 +0900

    [Feat] 존재하지 않는 상품에 대한 에러코드 추가 (#28)

--- resource-server/src/main/java/com/ryuneng/goldresource/global/exception/ErrorCode.java
+++ resource-server/src/main/java/com/ryuneng/goldresource/global/exception/ErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    UNKNOWN_USER(HttpStatus.BAD_REQUEST, "검증되지 않은 사용자입니다.");
+    UNKNOWN_USER(HttpStatus.BAD_REQUEST, "검증되지 않은 사용자입니다."),
+
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 상품입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
