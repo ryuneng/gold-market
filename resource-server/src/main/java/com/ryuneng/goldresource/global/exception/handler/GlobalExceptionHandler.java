499890d5e8be7173e485cdcc884aee528a4ccc93
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-11 12:11:26 +0900

    [Feat] 자원 서버 예외처리 관련 클래스 생성

---
+++ resource-server/src/main/java/com/ryuneng/goldresource/global/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,88 @@
+package com.ryuneng.goldresource.global.exception.handler;
+
+import com.ryuneng.goldresource.global.exception.CustomException;
+import com.ryuneng.goldresource.global.exception.ErrorCode;
+import com.ryuneng.goldresource.global.exception.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // CustomException
+    @ExceptionHandler(value = {CustomException.class})
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+
+        log.error("handleCustomException", e);
+
+        ErrorCode errorCode = e.getErrorCode();
+        String message = e.getMessage();
+
+        ErrorResponse response = ErrorResponse.of(
+                errorCode.getHttpStatus(),
+                message
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    // Validation Exception 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(MethodArgumentNotValidException e) {
+
+        log.error("MethodArgumentNotValidException : {}", e.getMessage());
+
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage() // 첫번째 예외 메시지만 반환
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleSqlException(SQLIntegrityConstraintViolationException e) {
+
+        log.error("SQLIntegrityConstraintViolationException : {}", e.getMessage());
+
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.BAD_REQUEST,
+                e.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+
+        log.error("IllegalArgumentException : {}", e.getMessage());
+
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.BAD_REQUEST,
+                e.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException e) {
+
+        log.error("NullPointerException : {}", e.getMessage());
+
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.BAD_REQUEST,
+                e.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+}
