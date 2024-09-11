f965faf5dd6ef9eb31a3049a9da14898fe1318d4
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-11 21:49:09 +0900

    [Feat] 존재하지 않는 주문에 대한 에러코드 추가 (#32)

--- resource-server/src/main/java/com/ryuneng/goldresource/global/exception/ErrorCode.java
+++ resource-server/src/main/java/com/ryuneng/goldresource/global/exception/ErrorCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     UNKNOWN_USER(HttpStatus.BAD_REQUEST, "검증되지 않은 사용자입니다."),
-
-    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 상품입니다.");
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
