0321f19278cdc49dcb21a02cb538413baa6429b9
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-10-02 13:27:13 +0900

    [Docs] 인증 서버 Swagger API 문서 추가 (#38)

--- auth-server/src/main/java/com/ryuneng/goldauth/domain/user/entity/User.java
+++ auth-server/src/main/java/com/ryuneng/goldauth/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package com.ryuneng.goldauth.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import com.ryuneng.goldauth.global.entity.BaseEntity;
+import org.hibernate.annotations.Comment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,18 +19,23 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("사용자 ID")
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @Comment("사용자 이메일")
     private String email;
 
     @Column(nullable = false)
+    @Comment("사용자 비밀번호")
     private String password;
 
     @Column(nullable = false)
+    @Comment("사용자 닉네임")
     private String nickname;
 
     @Column(nullable = false)
+    @Comment("사용자 전화번호")
     private String phoneNumber;
 
     @ElementCollection(fetch = FetchType.EAGER)
